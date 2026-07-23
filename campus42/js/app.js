46a17a1b4bb24d79039f126b387266df353f7edc
neehanazer <neehanaz226@gmail.com>
2026-07-23 11:52:44 +0530

    done

--- campus42/js/app.js
+++ campus42/js/app.js
@@ -1,22 +1,32 @@
 var app = angular.module("campus360", []);
 
 app.controller("dashboardController", function ($scope) {
-  $scope.collegename = "FISAT";
+  // Imported updated initial values from 2nd JS
+  $scope.collegename = "FiSaT";
+  $scope.collegeId = "FISAT-2026";
   $scope.totalstudents = 1200;
-  $scope.totalfaculty = 85;
-
+  $scope.totalfaculty = 120; // Updated from 85 to 120
   $scope.students = ["Shahid", "John", "Allen", "Shaun"];
-
-  $scope.fees = 2500;
+  $scope.showstudents = true;
+  $scope.fees = 25600; // Updated from 2500 to 25600
   $scope.today = new Date();
 
-  $scope.departments = ["CSE", "MCA", "MBA"];
-
-  $scope.selectedDepartment = "CSE";
-
-  $scope.showstudents = true;
+  $scope.departments = ["CSE", "MCA", "MBA", "EEE", "ECE", "BCA", "IMCA"];
+  $scope.selectedDepartment = "MCA"; // Default updated to MCA
 
   $scope.addStudent = function () {
     $scope.totalstudents++;
   };
+
+  $scope.isDisable = true;
+  $scope.allowAdmission = function () {
+    $scope.isDisable = false; // Directly enables admission (from 2nd JS logic)
+  };
+
+  $scope.isReadOnly = true;
+  $scope.toggleReadOnly = function () {
+    $scope.isReadOnly = !$scope.isReadOnly;
+  };
+
+  $scope.student = {};
 });
